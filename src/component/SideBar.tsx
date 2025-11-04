a6e77faecf04841afc6a931e050b27e6e3fdc587
Saleh Moosavi <salehmoosavi77@gmail.com>
2025-11-04 12:33:33 +0330

    feat: enhance sidebar navigation and update home page layout
    - Added react-icons for improved visual representation in the SideBar component.
    - Refactored SideBar layout for better responsiveness and aesthetics.
    - Introduced a new home image for the background in the Home page.
    - Updated Home page content and styling for a more engaging user experience.

--- src/component/SideBar.tsx
+++ src/component/SideBar.tsx
@@ -1,11 +1,37 @@
+import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { TbNumbers, TbTicTac } from "react-icons/tb";
 
 export default function SideBar() {
   return (
-    <aside className="fixed inset-y-0 start-0 bg-white p-5 flex flex-col gap-5">
-      <Link to="/">Home</Link>
-      <Link to="/tic-tac-toe">Tic-Tac-Toe</Link>
-      <Link to="/guess-number">Guess-Number</Link>
+    <aside className="fixed inset-x-0 md:inset-y-0 md:inset-x-auto start-0 bg-white/20 p-2 gap-1 flex md:flex-col m-2 rounded-lg backdrop-blur-md border-t border-r">
+      <Link
+        to="/"
+        className="group bg-blue-100 p-3 relative rounded-lg me-auto md:mb-auto"
+      >
+        <FaHome className="size-5" />
+        <span className="absolute invisible md:visible ms-0 left-full top-1/2 -translate-y-1/2 whitespace-nowrap bg-white opacity-0 group-hover:opacity-100 group-hover:ms-5 transition-all duration-200 text-xs p-2 rounded-lg">
+          Home
+        </span>
+      </Link>
+      <Link
+        to="/tic-tac-toe"
+        className="group bg-blue-100 p-3 relative rounded-lg"
+      >
+        <TbTicTac className="size-5" />
+        <span className="absolute invisible md:visible ms-0 left-full top-1/2 -translate-y-1/2 whitespace-nowrap bg-white opacity-0 group-hover:opacity-100 group-hover:ms-5 transition-all duration-200 text-xs p-2 rounded-lg">
+          Tic-Tac-Toe
+        </span>
+      </Link>
+      <Link
+        to="/guess-number"
+        className="group bg-blue-100 p-3 relative rounded-lg"
+      >
+        <TbNumbers className="size-5" />
+        <span className="absolute invisible md:visible ms-0 left-full top-1/2 -translate-y-1/2 whitespace-nowrap bg-white opacity-0 group-hover:opacity-100 group-hover:ms-5 transition-all duration-200 text-xs p-2 rounded-lg">
+          Guess-The-Number
+        </span>
+      </Link>
     </aside>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+import BlurBackGround from "../component/BlurBackGround";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen text-white bg-gradient-to-tr from-slate-700 to to-slate-900">
-      <section className="p-5 w-1/3 bg-gradient-to-tr from-emerald-700 to-emerald-500 rounded-xl text-center">
-        <h2 className="text-xl font-bold">Welcome to Here</h2>
-        <h4 className="font-semibold text-sm">Choose One You Need</h4>
-        <article className="mt-5 flex gap-5 *:px-4 *:py-2 *:border *:rounded-lg">
-          <Link to="/tic-tac-toe">Tic Tac Toe</Link>
-          <Link to="/guess-number">Guess The Number</Link>
+    <div className="flex justify-center items-center w-screen h-screen text-white">
+      <BlurBackGround src="/home.jpeg" />
+      <section className="p-2 md:p-5 w-3/4 md:w-1/2 lg:w-1/3 bg-gradient-to-tr from-rose-600 to-blue-600 rounded-xl text-center">
+        <h2 className="md:text-xl font-bold">Welcome to Hobby Verse</h2>
+        <article className="mt-5 flex justify-between gap-5">
+          <Link
+            to="/tic-tac-toe"
+            className="hover:bg-yellow-400 hover:text-black transition-all duration-200 px-4 py-2 border rounded-lg text-xs md:text-sm whitespace-nowrap"
+          >
+            Tic Tac Toe
+          </Link>
+          <Link
+            to="/guess-number"
+            className="hover:bg-yellow-400 hover:text-black transition-all duration-200 px-4 py-2 border rounded-lg text-xs md:text-sm whitespace-nowrap"
+          >
+            Guess The Number
+          </Link>
         </article>
       </section>
     </div>
