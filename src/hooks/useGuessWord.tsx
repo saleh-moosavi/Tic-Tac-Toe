baf8054cab2c24e58a776bdd190fe1147a3a87bf
Saleh Moosavi <salehmoosavi77@gmail.com>
2026-04-09 18:52:03 +0330

    feat : guessWord
    - add input to enter character
    - fix logic to show character if is true
    - add form styles

--- src/hooks/useGuessWord.tsx
+++ src/hooks/useGuessWord.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
 import { guessWordAllwords } from "../constants";
+import { useEffect, useRef, useState } from "react";
 import { getSecureRandomInt } from "../utils/randomInteger";
 
 export default function useGuessWord() {
+  const maxTries = useRef<number>(0);
+  const [gameState, setGameState] = useState<"win" | "lose" | "unknown">(
+    "unknown",
+  );
   const [word, setWord] = useState<
     {
       char: string;
@@ -21,14 +25,40 @@ export default function useGuessWord() {
     const finalWord = splitedWord.map((char) => {
       return { char: char, isEntered: false };
     });
+    maxTries.current = finalWord.length;
     setWord(finalWord);
+    setGameState("unknown");
   };
 
-  const checkEnteredChar = (index: number) => {
+  const checkEnteredChar = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const guessInputValue = formData.get("guess");
+    if (gameState !== "unknown" || guessInputValue == "") return;
     const wordCopy = [...word];
-    wordCopy[index].isEntered = true;
+    wordCopy.map((item) => {
+      if (
+        guessInputValue?.toString().toLocaleLowerCase() ==
+        item.char.toLocaleLowerCase()
+      ) {
+        item.isEntered = true;
+      }
+    });
     setWord(wordCopy);
+    maxTries.current -= 1;
+    const remainsChars = wordCopy.filter((item) => item.isEntered == false);
+
+    if (maxTries.current == 0 && remainsChars.length > 0) {
+      setGameState("lose");
+      return;
+    } else if (remainsChars.length == 0) {
+      setGameState("win");
+      return;
+    } else {
+      setGameState("unknown");
+      return;
+    }
   };
 
-  return { word, handleReset, checkEnteredChar };
+  return { word, gameState, handleReset, checkEnteredChar };
 }
--- src/pages/GuessWord.tsx
+++ src/pages/GuessWord.tsx
@@ -2,19 +2,26 @@ import Btn from "../component/Btn";
 import Card from "../component/Card";
 import styles from "./GuessWord.module.scss";
 import useGuessWord from "../hooks/useGuessWord";
+import Input from "../component/Input";
 
 export default function GuessWord() {
-  const { word, handleReset, checkEnteredChar } = useGuessWord();
+  const { word, gameState, handleReset, checkEnteredChar } = useGuessWord();
   return (
     <Card bgSrc="/home.jpeg" title="Guess The Word">
       <section className={styles.section}>
-        <article className={styles.article}>
+        <article className={styles.wordArticle}>
           {word.map((item, index) => (
-            <Btn key={index} onClick={() => checkEnteredChar(index)}>
+            <Btn key={index}>
               <p>{item.isEntered ? item.char : ".."}</p>
             </Btn>
           ))}
         </article>
+        <form className={styles.form} onSubmit={checkEnteredChar}>
+          <Input type="string" maxLength={1} name="guess" />
+          <Btn type="submit">Check</Btn>
+        </form>
+        <hr />
+        <p>{gameState}</p>
         <Btn onClick={handleReset}>Reset</Btn>
       </section>
     </Card>
