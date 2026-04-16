7b4af1a84ff121878ce8bd61713418492925d099
Saleh Moosavi <salehmoosavi77@gmail.com>
2026-04-16 19:12:39 +0330

    feat : calculator
    - add click handler to component
    - change shape of calculatorBtns

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -4,21 +4,37 @@ import Input from "../component/Input";
 import { CgBackspace } from "react-icons/cg";
 import { calculatorBtns } from "../constants";
 import styles from "./Calculator.module.scss";
+import { useRef, useState } from "react";
 
 export default function Calculator() {
+  const [inputValue, setinputValue] = useState("");
+  const [resultValue, setResultValue] = useState("");
+  const enteredValueRef = useRef<string[]>([]);
+
+  const handleClick = (value: string) => {
+    enteredValueRef.current.push(value);
+    if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(value)) {
+      setResultValue(eval(enteredValueRef.current.join("")));
+    } else {
+      setResultValue("");
+    }
+    setinputValue(enteredValueRef.current.join(""));
+  };
   return (
     <Card bgSrc="/home.jpeg" title="Calculator">
       <section className="space-y-5">
-        <Input type="string" />
+        <Input value={inputValue} type="string" />
         <article className={styles.resultArticle}>
-          <p>Result</p>
+          <p>{resultValue}</p>
           <p>
             <CgBackspace className="size-4" />
           </p>
         </article>
         <article className={styles.buttonsArticle}>
-          {calculatorBtns.map((item) => (
-            <Btn key={item}>{item}</Btn>
+          {calculatorBtns.map((key) => (
+            <Btn onClick={() => handleClick(key.value)} key={key.id}>
+              {key.icon}
+            </Btn>
           ))}
         </article>
       </section>
